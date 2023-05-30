c27f4de3fa4f0c1a695594d1efdfdd707be613bd
Betico1928 <betoveluis@gmail.com>
2023-05-30 13:34:47 -0500

    Mostrar las materias con los resultados y los creditos en una tabla

--- Calcular_Promedio_Ponderado/src/main/kotlin/Main.kt
+++ Calcular_Promedio_Ponderado/src/main/kotlin/Main.kt
@@ -6,29 +6,43 @@ fun main(args: Array<String>)
 
     println("¿Cuántas materias quieres calcular?")
     val numMaterias = input.nextInt()
-    input.nextLine()
+    input.nextLine()  // consume el '\n' que no consumió nextInt()
 
     var sumNotaPorCreditos = 0.0
     var sumCreditos = 0
+
+    val materias = mutableListOf<String>()
+    val notas = mutableListOf<Double>()
+    val creditos = mutableListOf<Int>()
 
     for (i in 1..numMaterias)
     {
         println("Por favor ingresa el nombre de la materia $i:")
         val materia = input.nextLine()
+        materias.add(materia)
 
         println("Por favor ingresa la nota final en la materia '$materia':")
         val nota = input.nextDouble()
         input.nextLine()  // consume el '\n' que no consumió nextDouble()
+        notas.add(nota)
 
         println("Por favor ingresa cuántos créditos vale la materia '$materia':")
-        val creditos = input.nextInt()
+        val credito = input.nextInt()
         input.nextLine()  // consume el '\n' que no consumió nextInt()
+        creditos.add(credito)
 
-        sumNotaPorCreditos += nota * creditos
-        sumCreditos += creditos
+        sumNotaPorCreditos += nota * credito
+        sumCreditos += credito
+    }
+
+    println("\nTabla de Materias, Notas y Créditos:")
+    for (i in 0 until numMaterias)
+    {
+        println("${materias[i]}: Nota = ${notas[i]}, Créditos = ${creditos[i]}")
     }
 
     val promedioPonderado = sumNotaPorCreditos / sumCreditos
-    println("El promedio ponderado de las materias es: $promedioPonderado")
+    println("\nEl promedio ponderado de las materias es: $promedioPonderado")
 }
+
 
