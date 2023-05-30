25d896acf5757c37d199299f7020d762e23ea01a
Betico1928 <betoveluis@gmail.com>
2023-05-30 12:48:11 -0500

    Se pueden poner espacios en los nombres de las materias.

--- Calcular_Promedio_Ponderado/src/main/kotlin/Main.kt
+++ Calcular_Promedio_Ponderado/src/main/kotlin/Main.kt
@@ -6,6 +6,7 @@ fun main(args: Array<String>)
 
     println("¿Cuántas materias quieres calcular?")
     val numMaterias = input.nextInt()
+    input.nextLine()
 
     var sumNotaPorCreditos = 0.0
     var sumCreditos = 0
@@ -13,13 +14,15 @@ fun main(args: Array<String>)
     for (i in 1..numMaterias)
     {
         println("Por favor ingresa el nombre de la materia $i:")
-        val materia = input.next()
+        val materia = input.nextLine()
 
-        println("Por favor ingresa la nota final en la materia $materia:")
+        println("Por favor ingresa la nota final en la materia '$materia':")
         val nota = input.nextDouble()
+        input.nextLine()  // consume el '\n' que no consumió nextDouble()
 
-        println("Por favor ingresa cuántos créditos vale la materia $materia:")
+        println("Por favor ingresa cuántos créditos vale la materia '$materia':")
         val creditos = input.nextInt()
+        input.nextLine()  // consume el '\n' que no consumió nextInt()
 
         sumNotaPorCreditos += nota * creditos
         sumCreditos += creditos
@@ -28,3 +31,4 @@ fun main(args: Array<String>)
     val promedioPonderado = sumNotaPorCreditos / sumCreditos
     println("El promedio ponderado de las materias es: $promedioPonderado")
 }
+
